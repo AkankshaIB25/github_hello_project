0b4d1f3acb15bad094d2b63e95a52e3a9bff69c7
AkankshaIB25 <55702643+AkankshaIB25@users.noreply.github.com>
2021-03-02 11:30:40 +0530

    Adding math.py file

---
+++ math.py
@@ -0,0 +1,15 @@
+# Addition Function
+def add(x,y):
+	pass
+
+# Subtraction Function
+def subtract(x,y):
+    pass
+
+#Multiplication Function
+def multiply(x,y):
+	pass
+
+#Division Function
+def divide(x,y):
+    pass
